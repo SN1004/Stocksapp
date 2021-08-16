9701610274d5684f1cee8a961b1e0597180035a7
SN1004 <ce20btech11041@iith.ac.in>
2021-08-16 19:30:54 +0530

    Working prototype 1

---
+++ src/landing/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+
+/**
+ * Header used on the landing page.
+ */
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        Stocksapp
+      </h1>
+      <Link to="/login" className={styles.button}>
+        Logout
+      </Link>
+    </header>
+  );
+}
